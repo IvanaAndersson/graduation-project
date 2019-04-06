3b412a295b50509daa7454c8d4275d0b49ac9f75
Ivana Andersson <ivana.p.kostova@gmail.com>
2019-04-06 22:55:15 +0300

    made reposnive for pointer fine

--- index.js
+++ index.js
@@ -8,25 +8,31 @@ const projectsSection = document.getElementById("projects");
 const projectsLink = document.getElementById("projects-link");
 const contactsSection = document.getElementById("contacts");
 const contactsLink = document.getElementById("contacts-link");
+const menuCheckbox = document.getElementById("nav-main-toggle");
 
 aboutLink.addEventListener("click", event => {
   event.preventDefault();
   aboutSection.scrollIntoView({ behavior: "smooth", block: "start" });
+  menuCheckbox.checked = false;
 });
 servicesLink.addEventListener("click", event => {
   event.preventDefault();
   servicesSection.scrollIntoView({ behavior: "smooth", block: "start" });
+  menuCheckbox.checked = false;
 });
 projectsLink.addEventListener("click", event => {
   event.preventDefault();
   projectsSection.scrollIntoView({ behavior: "smooth", block: "start" });
+  menuCheckbox.checked = false;
 });
 contactsLink.addEventListener("click", event => {
   event.preventDefault();
   contactsSection.scrollIntoView({ behavior: "smooth", block: "start" });
+  menuCheckbox.checked = false;
 });
 homeLink.addEventListener("click", event => {
   event.preventDefault();
   window.scroll({ behavior: "smooth", top: 0 });
+  menuCheckbox.checked = false;
 });
 
