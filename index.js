6d52efb8921b0641613a149683ad2df355016433
Ivana Andersson <ivana.p.kostova@gmail.com>
2020-07-19 18:26:52 +0300

    refactor index.js

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-const homeLink = document.getElementById("home-link");
-
-const aboutSection = document.getElementById("about");
-const aboutLink = document.getElementById("about-link");
-const servicesSection = document.getElementById("services");
-const servicesLink = document.getElementById("services-link");
-const projectsSection = document.getElementById("projects");
-const projectsLink = document.getElementById("projects-link");
-const contactsSection = document.getElementById("contacts");
-const contactsLink = document.getElementById("contacts-link");
 const menuCheckbox = document.getElementById("nav-main-toggle");
+//references to all links
+const homeLink = document.getElementById("home-link");
+const aboutLink = document.getElementById("about-link");
+const servicesLink = document.getElementById("services-link");
+const projectsLink = document.getElementById("projects-link");
+const contactsLink = document.getElementById("contacts-link");
+const links = [homeLink, aboutLink, servicesLink, projectsLink, contactsLink];
 
-aboutLink.addEventListener("click", event => {
-  event.preventDefault();
-  aboutSection.scrollIntoView({ behavior: "smooth", block: "start" });
-  menuCheckbox.checked = false;
-});
-servicesLink.addEventListener("click", event => {
-  event.preventDefault();
-  servicesSection.scrollIntoView({ behavior: "smooth", block: "start" });
-  menuCheckbox.checked = false;
-});
-projectsLink.addEventListener("click", event => {
-  event.preventDefault();
-  projectsSection.scrollIntoView({ behavior: "smooth", block: "start" });
-  menuCheckbox.checked = false;
-});
-contactsLink.addEventListener("click", event => {
-  event.preventDefault();
-  contactsSection.scrollIntoView({ behavior: "smooth", block: "start" });
-  menuCheckbox.checked = false;
-});
-homeLink.addEventListener("click", event => {
-  event.preventDefault();
-  window.scroll({ behavior: "smooth", top: 0 });
-  menuCheckbox.checked = false;
-});
+//references to all sections
+const aboutSection = document.getElementById("about");
+const servicesSection = document.getElementById("services");
+const projectsSection = document.getElementById("projects");
+const contactsSection = document.getElementById("contacts");
 
+const linksToSections = {
+  about: aboutSection,
+  services: servicesSection,
+  projects: projectsSection,
+  contacts: contactsSection,
+};
+
+const scrollToSection = (section) => {
+  section.scrollIntoView({ behavior: "smooth", block: "start" });
+  menuCheckbox.checked = false;
+};
+
+links.forEach((link) => {
+  const sectionName = link.id.split("-")[0];
+
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (sectionName === "home") {
+      window.scroll({ behavior: "smooth", top: 0 });
+    } else {
+      scrollToSection(linksToSections[sectionName]);
+    }
+  });
+});
